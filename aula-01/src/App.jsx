478b814ac88506957dce36d17271b1166c713a53
DiegodOliveira <ciprianodiegooliveira@gmail.com>
2024-03-05 19:46:42 -0300

    Multiplicacao e subtracao feita

--- aula-01/src/App.jsx
+++ aula-01/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Exercicio1Adicao from './components/Exercicio1Adicao'
+import Exercicio1Multiplicacao from './components/Exercicio1Multiplicacao'
+import Exercicio1Subtracao from './components/Exercicio1Subtracao'
 
 const App = () => {
   return (
     <div>
       <Exercicio1Adicao num1={2} num2={3}/>
+      <Exercicio1Multiplicacao num1={7} num={4}/>
+      <Exercicio1Subtracao num1={30} num2={20}/>
     </div>
-  )
+    )
 }
+
 
 export default App

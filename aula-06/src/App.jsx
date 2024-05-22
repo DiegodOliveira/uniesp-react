c6924f20edd4cf3fe3e67ebddc646a222a0903c6
Diego de Oliveira Santana Cipriano <112182138+DiegodOliveira@users.noreply.github.com>
2024-05-22 20:30:16 -0300

    aula-06

--- aula-06/src/App.jsx
+++ aula-06/src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Afaculdade from './pages/Afaculdade'
+import Noticias from './pages/Noticias'
+import DpoLgpd from './pages/DpoLgpd'
+import Inicial from './pages/Inicial'
+import Navbar from './components/Navbar'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    
+    <BrowserRouter>
+      <img src='uniesp.png' width={50} height={50}/>
+      <Navbar/>
+      <h1>Título da página</h1>
+      <Routes>
+        <Route path='/' element={<Inicial/>} />
+        <Route path='/a-faculdade' element={<Afaculdade/>} />
+        <Route path='/noticias' element={<Noticias/>} />
+        <Route path='/dpo-lgpd' element={<DpoLgpd/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ aula-06/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <Link to="/">Página Inicial</Link><br />
+        <Link to="/noticias">Notícias </Link><br />
+        <Link to="/dpolgpd">DpoLgpd</Link><br />
+        <Link to="/a-faculdade">A Faculdade</Link><br />
+
+    </nav>
+  )
+}
+
+export default Navbar
